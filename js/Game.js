1d90ed3623d0b076c7e432cf5078d72d31d905f4
Bosris <jamesallstar21@gmail.com>
2019-05-02 19:35:57 -0400

    second commit

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,18 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+class Game {
+  constructor() {
+    this.missed = 0;
+    this.phrases = createPhrases();
+    this.activePhrase = null;
+  }
+  createPhrases() {
+    return [
+      "the sky is blue",
+      "I want to go to school",
+      "life is like a box of chocolates",
+      "it is what it is"
+    ];
+  }
+}
